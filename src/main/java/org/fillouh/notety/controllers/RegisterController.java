9b968a82c43598e455633f5fea5138f1460c2bdd
Filippo Leonelli <300717@studenti.unimore.it>
2024-05-12 22:00:14 +0200

    clear and initialize method update

--- src/main/java/org/fillouh/notety/controllers/RegisterController.java
+++ src/main/java/org/fillouh/notety/controllers/RegisterController.java
@@ -1,18 +1,23 @@
 package org.fillouh.notety.controllers;
 
+import javafx.collections.FXCollections;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
 import org.fillouh.notety.Notety;
 
 import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
 
-public class RegisterController {
+public class RegisterController implements Initializable {
     @FXML
     private PasswordField addressField;
 
@@ -61,7 +66,8 @@ public class RegisterController {
     @FXML
     private TextField usernameField;
 
-
+    @FXML
+    private AnchorPane layer;
 
     public void back() throws IOException {
         backButton.getScene().getWindow().hide();
@@ -70,5 +76,18 @@ public class RegisterController {
         Scene scene=new Scene(fxmlLoader.load());
         stage.setScene(scene);
         stage.show();
+    }
+
+    public void clear(){
+        for (javafx.scene.Node node : layer.getChildren()) {
+            if (node instanceof TextField tx) {
+                tx.clear();
+            }
+        }
+    }
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        genderBox.setItems(FXCollections.observableArrayList("Male","Female","Other"));
     }
 }
