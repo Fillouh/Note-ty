61e7a4c17e6ba52281d1043f9129f12d3d4840ff
Filippo Leonelli <300717@studenti.unimore.it>
2024-05-12 18:47:19 +0200

    back method created

--- src/main/java/org/fillouh/notety/controllers/RegisterController.java
+++ src/main/java/org/fillouh/notety/controllers/RegisterController.java
@@ -1,10 +1,16 @@
 package org.fillouh.notety.controllers;
 
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+import org.fillouh.notety.Notety;
+
+import java.io.IOException;
 
 public class RegisterController {
     @FXML
@@ -57,7 +63,12 @@ public class RegisterController {
 
 
 
-    public void back(){
-        
+    public void back() throws IOException {
+        backButton.getScene().getWindow().hide();
+        FXMLLoader fxmlLoader = new FXMLLoader(Notety.class.getResource("views/login-view.fxml"));
+        Stage stage=new Stage();
+        Scene scene=new Scene(fxmlLoader.load());
+        stage.setScene(scene);
+        stage.show();
     }
 }
