d560a7cc6da3e32d3dd78d54b0c1d218ae609034
Filippo Leonelli <300717@studenti.unimore.it>
2024-06-04 17:24:23 +0200

    implementation of the actual user's label in DashboardController

--- src/main/java/org/fillouh/notety/controllers/DashboardController.java
+++ src/main/java/org/fillouh/notety/controllers/DashboardController.java
@@ -1,13 +1,46 @@
 package org.fillouh.notety.controllers;
 
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import org.fillouh.notety.general_classes.LoggedStudent;
+import javafx.fxml.FXML;
 
-public class DashboardController {
+import java.net.URL;
+import java.util.ResourceBundle;
 
-    //DA ANDARE A RESETTARE QUANDO FACCIO IL LOGOUT
+public class DashboardController implements Initializable {
+
+
+    @FXML
+    private Button accountButton;
+
+    @FXML
+    private Button activitiesButton;
+
+    @FXML
+    private Label currentuserLabel;
+
+    @FXML
+    private Button gradesButton;
+
+    @FXML
+    private Button logoutButton;
+
+    @FXML
+    private Label logstudString;
+
+    @FXML
+    private Button notificationsButton;
+
+
+    //DA ANDARE A RESETTARE QUANDO FACCIO IL LOGOUT COME IL TESTO NEL CAMPO STRINGA
     public static LoggedStudent actualUser;
 
 
-
-
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        logstudString.setText(actualUser.username);
+        logstudString.setVisible(true);
+    }
 }
