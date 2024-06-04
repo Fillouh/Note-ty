aeab327ea4f3e905a6a926eb02af097d0a533826
Filippo Leonelli <300717@studenti.unimore.it>
2024-06-04 17:31:04 +0200

    logout method in DashboardController implemented

--- src/main/java/org/fillouh/notety/controllers/DashboardController.java
+++ src/main/java/org/fillouh/notety/controllers/DashboardController.java
@@ -1,11 +1,16 @@
 package org.fillouh.notety.controllers;
 
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.stage.Stage;
+import org.fillouh.notety.Notety;
 import org.fillouh.notety.general_classes.LoggedStudent;
 import javafx.fxml.FXML;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
@@ -37,6 +42,17 @@ public class DashboardController implements Initializable {
     //DA ANDARE A RESETTARE QUANDO FACCIO IL LOGOUT COME IL TESTO NEL CAMPO STRINGA
     public static LoggedStudent actualUser;
 
+
+    public void logout() throws IOException {
+        logstudString.setText("");
+        logstudString.setVisible(false);
+        logoutButton.getScene().getWindow().hide();
+        FXMLLoader fxmlLoader = new FXMLLoader(Notety.class.getResource("views/login-view.fxml"));
+        Stage stage=new Stage();
+        Scene scene=new Scene(fxmlLoader.load());
+        stage.setScene(scene);
+        stage.show();
+    }
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
